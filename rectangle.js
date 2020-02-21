09c54aa4bad09e8ae67ac26837b1e48c26f74184
lvshufeng520 <1002463595@qq.com>
2020-02-21 09:11:47 +0800

    new

---
+++ rectangle.js
@@ -0,0 +1,30 @@
+$(
+    
+    function(){
+        function roundFractional(x,n){
+            return Math.round(x * Math.pow(10,n)) / Math.pow(10,n);
+        }
+        //get dom elem
+        var $width = $('#width'),
+            $height = $('#height'),
+            $btnCal = $('#calculate'),
+            $perimeter = $('#perimeter'),
+            $area = $('#area');
+        /*calc button click event*/
+        $btnCal.click(function(){
+            //get value
+            var w = Number($width.val()),
+                h = Number($height.val());
+            
+            // calculate
+            var p = 2 * (w + h),
+                a = w*h;
+            
+            //output
+            $perimeter.val(roundFractional(p,2));
+            $area.val(roundFractional(a,2));
+        })    
+    },
+
+    
+);
